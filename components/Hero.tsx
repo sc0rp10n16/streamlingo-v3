6b978189a1322f5a8416646039b54af729d1ffa8
sc0rp10n16 <akshithmysa.physics@gmail.com>
2024-09-11 08:46:08 +0530

    Setup done

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+
+
+const Hero = () => {
+  return (
+    <div>
+        
+            <div className="items-center justify-center rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5 p-5">
+                <h1 className="text-center font-bold text-7xl text-white">STREAMLINGO</h1>
+                <p className="text-gray-200 font-semibold text-2xl">Making world the better place!</p>
+            </div>
+        
+    </div>
+  )
+}
+export default Hero
